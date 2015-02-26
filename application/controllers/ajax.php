123cea55a6a876eb37a65c54fe38d7ee75daf448
Andy Awidarto <andy.awidarto@gmail.com>
2015-02-26 15:54:40 +0700

    email

--- application/controllers/ajax.php
+++ application/controllers/ajax.php
@@ -812,9 +812,9 @@ class Ajax extends Application
                 //print_r($app);
 
                 if($app->notify_on_new_order == 1){
-                    if(valid_email($in->email)){
+                    //if(valid_email($in->email)){
                         send_notification('New Delivery Order - Jayon Express COD Service',$in->email,$app->cc_to,$app->reply_to,'order_submit',$nedata,null);
-                    }
+                    //}
                 }
 
                 if($is_new == true){
