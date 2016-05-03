7f3bb4ea6d319779efcc3f56b8c149639c0553e6
Andy Awidarto <andy.awidarto@gmail.com>
2016-05-03 20:36:53 +0700

    incoming query

--- application/controllers/admin/delivery.php
+++ application/controllers/admin/delivery.php
@@ -179,8 +179,8 @@ class Delivery extends Application
 		}
 
 		$this->db
-			->where($this->config->item('incoming_delivery_table').'.merchant_id',$this->session->userdata('userid'))
 			->and_()->group_start()
+            ->where($this->config->item('incoming_delivery_table').'.merchant_id',$this->session->userdata('userid'))
             ->where($this->config->item('incoming_delivery_table').'.pending_count < ',1)
 			->where($this->config->item('incoming_delivery_table').'.status',$this->config->item('trans_status_new'))
 			->or_where($this->config->item('incoming_delivery_table').'.status',$this->config->item('trans_status_confirmed'))
